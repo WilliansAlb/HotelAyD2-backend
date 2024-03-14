530b9fe5683af0e36b6d5e763402098247bfa979
César Tzoc <csartalva01@gmail.com>
2024-03-13 23:58:03 -0600

    Initial commit
    - adding db config
    - adding flyway config
    - adding demo controller
    - adding base dependencies

---
+++ src/main/java/com/ayd2/hotel/HotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayd2.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayd2/hotel/HotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayd2.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
