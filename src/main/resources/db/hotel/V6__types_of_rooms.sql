c8ae6cff7bba7e549404299bdfcd32dacdb5ade1
César Tzoc <csartalva01@gmail.com>
2024-03-19 22:23:41 -0600

    migration for types of rooms

---
+++ src/main/resources/db/hotel/V6__types_of_rooms.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.htl_room_type (room_type_id, room_type_name, number_of_beds, price) VALUES (5001 , 'Junior', 1, 100.000000);
+INSERT INTO public.htl_room_type (room_type_id, room_type_name, number_of_beds, price) VALUES (5002, 'Master', 3, 200.000000);
+INSERT INTO public.htl_room_type (room_type_id, room_type_name, number_of_beds, price) VALUES (5003, 'Lujo', 4, 500.000000);
